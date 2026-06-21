3805a8e938b8c942272a7a475ec3eee7061fb47a
Andrei Chenchik <andrei@chenchik.me>
2026-06-21 19:32:30 +0200

    Install binary as container-primer (no rename)

--- Formula/container-primer.rb
+++ Formula/container-primer.rb
@@ -12,7 +12,7 @@ class ContainerPrimer < Formula
   def install
     # Keep the binary and its *.bundle resources (loaded via Bundle.module) adjacent.
     libexec.install Dir["*"]
-    bin.install_symlink libexec/"ContainerPrimer" => "container-primer"
+    bin.install_symlink libexec/"container-primer"
   end
 
   def caveats
